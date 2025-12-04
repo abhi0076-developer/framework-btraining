9db0298eef0a1b0c2a428d587845654850a45243
Ankitha290603 <ankitha.hebbar30@gmail.com>
2025-12-04 17:21:27 +0530

    initial commit with project and app

--- Kavya_project/myproject/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('Hi, I’m Kavyanjali. I am passionate about coding and web development, especially working with Python and Django. I enjoy building projects that solve real-world problems and continuously learning new technologies to improve my skills.')
--- myintroduction/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('Hi, This is Ankitha S, a recent Computer Science Graduate from Rajarajeswari College of Engineering. I have a strong foundation in Python, SQL, and Web Technologies like HTML,CSS,Javascript and Django. I am passionate about building scalable and effective projects which is a combination of both creativity and innovation. I have built projects on IoT and AIML.I have got certifications on Python and DataScience. Apart from my Academics, I am good at sketching and dancing which helps me to stay creative and balanced.Finally my goal is to become a Backend Developer.')
