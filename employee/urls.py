dba73d0e8429f1d15a8f3cd1fe3d0154f7e64511
Ankitha290603 <ankitha.hebbar30@gmail.com>
2025-12-04 17:56:14 +0530

    Delete employee directory

--- employee/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns=[
-    path('',views.add_employee,name='home'),
-    path('add/',views.add_employee,name='add_employee.html'),
-    path('success/',views.success,name='success'),
-]
